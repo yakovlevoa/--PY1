1e6733dc7c2b4e03f3dca11a67ff330fa97528cf
yakovlevoa <115572772+yakovlevoa@users.noreply.github.com>
2022-10-18 19:47:27 +0300

    Update Лаба 3_1.py

--- Лабы/Лаба 3_1.py
+++ Лабы/Лаба 3_1.py
@@ -1,7 +1,9 @@
 src = not False and True or False and not True
 
 # TODO расписать упрощение выражения
-
+# True and True or False and False
+# True or False
+# True
 result = True
 
 print(src == result)
